1ea9448f005867d99121fe2c9a63ac399cbc75a4
jsvivien <manhhungacoustic@gmail.com>
2018-07-19 13:52:16 +0700

    Start Project

---
+++ app/src/main/java/com/example/tmh/servicereceivernotify/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.tmh.servicereceivernotify;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
